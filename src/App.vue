3e574b2f2bdd03770bbd64cd05fe245b55729e2f
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-02-13 19:19:18 +0100

    fix (AppMain & AppHeader) /fixed-search

--- src/App.vue
+++ src/App.vue
@@ -18,32 +18,39 @@ export default {
   methods:{
     getList(){
 
+      store.apiUrlMoviesSearch = 'https://api.themoviedb.org/3/search/movie?api_key=9cc9fcb5482c5e3d9b233e9976513ee0'
+      store.apiUrlTvSearch = 'https://api.themoviedb.org/3/search/tv?api_key=9cc9fcb5482c5e3d9b233e9976513ee0'
+
+      if(store.searchText !== ''){
+        store.apiUrlMoviesSearch += `&query=${store.searchText}`
+      }
+
+      if(store.searchText !== ''){
+        store.apiUrlTvSearch += `&query=${store.searchText}`
+      }
+
       axios
-      .get(store.apiUrlMovies)
+      .get(store.apiUrlMoviesSearch)
       .then((result) => {
         console.log(result.data)
         store.cardListMovies = result.data.results
       })
 
       axios
-      .get(store.apiUrlTv)
+      .get(store.apiUrlTvSearch)
       .then((result) => {
         console.log(result.data)
         store.cardListTvs = result.data.results 
       })
 
-      if(store.searchText !== ''){
-        store.apiUrlMovies += `&title=${searchText}`
-      }
 
-      if(store.searchText !== ''){
-        store.apiUrlTv += `&name=${searchText}`
-      }
+      
     }
   },
   mounted(){
     this.getList()
-    console.log(store.cardList)
+    console.log(store.cardListMovies)
+    console.log(store.cardListTv)
   }
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -3,7 +3,8 @@ import { reactive } from 'vue'
 export const store = reactive ({
     cardListMovies: [],
     cardListTvs: [],
-    apiUrl: 'https://developers.themoviedb.org/3',
+    apiUrlMoviesSearch: 'https://api.themoviedb.org/3/search/movie?api_key=9cc9fcb5482c5e3d9b233e9976513ee0',
+    apiUrlTvSearch: 'https://api.themoviedb.org/3/search/tv?api_key=9cc9fcb5482c5e3d9b233e9976513ee0',
     apiUrlMovies: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=9cc9fcb5482c5e3d9b233e9976513ee0',
     apiUrlTv: 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc&api_key=9cc9fcb5482c5e3d9b233e9976513ee0',
     searchText: ''
